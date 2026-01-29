bf9b4e7dadafff9fbaf2c52a49ef56913605ebe4
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-01-29 12:15:58 +0200

    Add NoteStorage file for mock testing

---
+++ src/main/java/temporary_note_storage/NoteStorage.java
@@ -0,0 +1,35 @@
+package temporary_note_storage;
+
+import model.Note;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteStorage {
+    private List<Note> notes;
+
+    public NoteStorage() {
+        notes = new ArrayList<>();
+    }
+
+    public void createNote(Note note) {
+        //implement yourselves
+    }
+
+    public void updateNote(Note note) {
+        //implement yourselves
+    }
+
+    public void deleteNote(Note note) {
+        //implement yourselves
+    }
+
+    public List<Note> getNotesByUser(int userId) {
+        List<Note> userNotes = new ArrayList<>();
+        for (Note note : notes) {
+            if (note.getUserId() == userId) {
+                userNotes.add(note);
+            }
+        }
+        return userNotes;
+    }
+}
