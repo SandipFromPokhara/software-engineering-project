6f067fead9427c93b9b213cff604e91ac415bf08
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-01-29 21:57:36 +0200

    Initialize controller class for login

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,4 +1,28 @@
 package controller;
 
-public class Controller {
+import javafx.scene.control.*;
+import javafx.fxml.FXML;
+import model.Note;
+
+public class LoginController {
+
+    @FXML private TextField usernameField;
+
+    @FXML private PasswordField passwordField;
+
+    @FXML private Button loginButton;
+
+    @FXML private Label errorLabel;
+
+    @FXML private Hyperlink signupLink;
+
+    private void handleLogin() {
+
+    }
+
+    private void initialize() {
+        loginButton.setDisable(true);
+        errorLabel.setDisable(true);
+
+    }
 }
