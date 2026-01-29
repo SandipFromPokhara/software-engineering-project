e9d871318b3e895a031fb353b7893d296d226c50
Twe He Gam Aung <Gam.TweHe@metropolia.fi>
2026-01-29 18:28:38 +0200

    Set up landing page

--- src/main/java/controller/Entry.java
+++ src/main/java/controller/Entry.java
@@ -1,4 +1,36 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+@SuppressWarnings("unused")
 public class Entry {
+
+    @FXML
+    private Button guestButton;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private void onLogin() {
+        System.out.println("Login button clicked");
+        // TODO: navigate to login screen
+    }
+
+    @FXML
+    private void onRegister() {
+        System.out.println("Register button clicked");
+        // TODO: navigate to register screen
+    }
+
+    @FXML
+    private void onContinueAsGuest() {
+        System.out.println("Continue as guest clicked");
+        // TODO: proceed as guest
+    }
+
 }
