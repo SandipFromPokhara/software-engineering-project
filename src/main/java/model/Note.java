c28e8f3fc1554c3f90480fc6d04813744a1f9056
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-01-29 11:54:37 +0200

    Update Note model with getters and setters, and a method to set updated content and timestamp

--- src/main/java/model/Note.java
+++ src/main/java/model/Note.java
@@ -1,8 +1,50 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Note {
     private int id;
-    private String noteTitle;
-    private String noteContent;
+    private static int idCounter = 1;
+    private String title;
+    private String content;
+    private int userId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
+    public Note(String title, String content, int userId) {
+        id = idCounter++;
+        this.title = title;
+        this.content = content;
+        this.userId = userId;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public int getId() { return id; }
+
+    public String getTitle() { return title; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public String getContent() { return content; }
+
+    public void setContent(String content) { this.content = content; }
+
+    /**
+     * Updates new content and syncs last created or modified timestamp.
+     */
+    public void updateContent(String newContent) {
+        this.content = newContent;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public int getUserId() { return userId; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+
+    @Override
+    public String toString() {
+        return "Note {Id: " + id + ", Title: '" + title + "', UserId: '" + userId + "', Timestamp: " + createdAt + "}";
+    }
 }
+
