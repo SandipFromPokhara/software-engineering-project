5decf066d54e3c0bac4b8c42e4f92477e75f9a47
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-01-30 13:00:57 +0200

    Add UI class for login, extend Application class and override start method

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -1,4 +1,23 @@
 package view;
 
-public class View {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class LoginView extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FXML/login_view.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Scene loginScene = new Scene(root);
+        stage.getIcons().add(new Image("/Logo-NoteVault.png"));
+        stage.setTitle("NoteVault");
+        stage.setScene(loginScene);
+        stage.show();
+    }
 }
