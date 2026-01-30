c14d6f677ac3c4c58a9fe7b4ad9055ce1119d27b
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-01-30 19:49:24 +0200

    Initial handle login with user authentication succesful, listeners added to username and password field in initialize(). Add helper methods for username and password fields and checkFields()

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -4,9 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.fxml.FXML;
 
+import javafx.scene.image.ImageView;
+import model.User;
 import repository.UserRepository;
 
 public class LoginController {
+
+    public ImageView leftImage;
+
+    public ImageView logo;
 
     private UserRepository userRepository;
 
@@ -20,20 +26,33 @@ public class LoginController {
 
     @FXML private Hyperlink signupLink;
 
+    private String getUsername() {
+        return usernameField.getText().trim();
+    }
+
+    private String getPassword() {
+        return passwordField.getText().trim();
+    }
+
+    private void checkFields() {
+        String username = getUsername();
+        String password = getPassword();
+        loginButton.setDisable(username.isEmpty() || password.isEmpty());
+    }
+
     @FXML
     private void handleLogin() {
-        String username = usernameField.getText().trim();
-        String password = passwordField.getText().trim();
-
-        if (username.isEmpty() || password.isEmpty()) {
-            errorLabel.setText("Invalid username or password");
-            return;
-        }
+        String username = getUsername();
+        String password = getPassword();
 
         boolean authenticated = userRepository.authenticateUser(username, password);
         if (authenticated) {
-            errorLabel.setText("");
+            errorLabel.setVisible(true);
+            errorLabel.setText("Login successful");
+
+            // navigate(dashboard);
         } else {
+            errorLabel.setVisible(true);
             errorLabel.setText("Invalid username or password");
         }
     }
@@ -44,10 +63,17 @@ public class LoginController {
         loginButton.setDisable(true);
         errorLabel.setVisible(false);
 
+        usernameField.textProperty().addListener((observable, oldValue, newValue) -> checkFields());
+
+        passwordField.textProperty().addListener((observable, oldValue, newValue) -> checkFields());
+
+        User user = new User("test", "first", "testuser", "abcd");
+        userRepository.addUser(user);
     }
 
     @FXML
-    private void handleSignup(ActionEvent event) {
+    private void handleSignUp(ActionEvent event) {
+
     }
 
 }
