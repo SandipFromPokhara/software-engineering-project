ff086c8ca616d992bea492b69804331b82d7a99c
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-01-30 11:35:52 +0200

    Initialize controller for login with initial handleLogin function

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -2,9 +2,12 @@ package controller;
 
 import javafx.scene.control.*;
 import javafx.fxml.FXML;
-import model.Note;
+
+import repository.UserRepository;
 
 public class LoginController {
+
+    private UserRepository userRepository;
 
     @FXML private TextField usernameField;
 
@@ -16,13 +19,29 @@ public class LoginController {
 
     @FXML private Hyperlink signupLink;
 
+    @FXML
     private void handleLogin() {
+        String username = usernameField.getText().trim();
+        String password = passwordField.getText().trim();
 
+        if (username.isEmpty() || password.isEmpty()) {
+            errorLabel.setText("Invalid username or password");
+            return;
+        }
+
+        boolean authenticated = userRepository.authenticateUser(username, password);
+        if (authenticated) {
+            errorLabel.setText("");
+        } else {
+            errorLabel.setText("Invalid username or password");
+        }
     }
 
+    @FXML
     private void initialize() {
+        userRepository = new UserRepository();
         loginButton.setDisable(true);
-        errorLabel.setDisable(true);
+        errorLabel.setVisible(false);
 
     }
 }
