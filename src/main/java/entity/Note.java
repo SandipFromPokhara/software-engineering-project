442d5a07480c72551753655cd7d2d66dde2624bb
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-01-31 23:00:28 +0200

    Add required setters and PrePersist+PreUpdate for created and updated time

---
+++ src/main/java/entity/Note.java
@@ -0,0 +1,65 @@
+package entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="notes")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="content")
+    private String content;
+
+    @Column(name="createdAt")
+    private LocalDateTime createdAt;
+
+    @Column(name="updatedAt")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "notebook_id", nullable = false)
+    private NoteBook notebook;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Note() {}
+
+    public Long getId() { return id; }
+
+    public String getTitle() { return title; }
+
+    public String getContent() { return content; }
+
+    public LocalDateTime getCreatedTime() { return createdAt; }
+
+    public LocalDateTime getUpdatedTime() { return updatedAt; }
+
+    public void setTitle(String newTitle) {
+        this.title = newTitle;
+    }
+
+    public void setContent(String newContent) {
+        this.content = newContent;
+    }
+}
