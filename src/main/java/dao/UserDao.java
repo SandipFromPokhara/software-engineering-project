315c1d9b095b7cb49a1bd6deaa93bb1ca111c09f
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-02-01 09:35:58 +0200

    Initialize user repository file for JPA persistence

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,23 @@
 package dao;
 
-public class DAO {
+import datasource.MariaDbJpaConnection;
+import entity.UserEntity;
+import jakarta.persistence.EntityManager;
+
+public class UserDao {
+
+    public void persist(UserEntity user) {
+        EntityManager em = MariaDbJpaConnection.createEntityManager();
+
+    }
+
+    public UserEntity findByUsername(UserEntity id) {
+        EntityManager em = MariaDbJpaConnection.createEntityManager();
+        return id;
+    }
+
+    public UserEntity findByEmail(UserEntity id) {
+        EntityManager em = MariaDbJpaConnection.createEntityManager();
+        return id;
+    }
 }
