602a1b690a51aff6cc8bc87525e34bc290a5589a
Swostika Lama <swostika.lama01@gmail.com>
2026-02-01 08:39:04 +0200

    Add CreateFilesGuest FXML and controller for file creation functionality

--- src/main/java/controller/GuestDashboardController.java
+++ src/main/java/controller/GuestDashboardController.java
@@ -1,15 +1,18 @@
 package controller;
 
-import javafx.scene.control.*;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+
 
 public class GuestDashboardController {
-    @FXML
-    private TextField titleBox;
-
-    @FXML
-    private TextArea contentBox;
 
     @FXML
     private Button home;
@@ -33,26 +36,48 @@ public class GuestDashboardController {
     private Label content;
 
     @FXML
+    private VBox centerPane;
+
+    @FXML
     private void handleHome() {
         System.out.println("Go to Home page");
     }
+
+    private void loadContent(String fxmlFile) {
+        try {
+            Parent view = FXMLLoader.load(
+                    getClass().getResource("/FXML/" + fxmlFile)
+            );
+
+            StackPane wrapper = new StackPane(view);
+            VBox.setVgrow(wrapper, Priority.ALWAYS);
+
+            //To make responsive
+            wrapper.prefWidthProperty().bind(centerPane.widthProperty());
+            wrapper.prefHeightProperty().bind(centerPane.heightProperty());
+
+            centerPane.getChildren().setAll(wrapper);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
     @FXML
     private void handleNewFiles() {
-        System.out.println("Go to create new files.");
-        // load login.fxml here
+        loadContent("CreateFilesGuest.fxml");
     }
 
     @FXML
     private void handleLogin() {
         System.out.println("Go to Login page");
-
     }
 
     @FXML
     private void handleRegister() {
         System.out.println("Go to Register page");
     }
-
-
-
 }
