f5248487f5a2d2a112eb0ddfc3feb07676a727e8
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-02-01 13:47:30 +0200

    Remove unused import

--- src/main/java/dao/user/UserDAO.java
+++ src/main/java/dao/user/UserDAO.java
@@ -3,6 +3,8 @@ package dao.user;
 import dao.baseDAO.GenericDAO;
 import entity.UserEntity;
 
+import java.util.List;
+
 public interface UserDAO extends GenericDAO<UserEntity, Long> {
     UserEntity findByUsername(String username);
 
