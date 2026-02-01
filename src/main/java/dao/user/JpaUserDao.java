f2df6bb731b1a76f93f1f071bbb52732c3f86d66
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-02-01 13:48:09 +0200

    Implement save, find, update and delete methods for Jpa entity

--- src/main/java/dao/user/JpaUserDao.java
+++ src/main/java/dao/user/JpaUserDao.java
@@ -1,20 +1,26 @@
-package dao;
+package dao.user;
 
 import datasource.MariaDbJpaConnection;
 import entity.UserEntity;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 
 import java.util.List;
 
-public class UserDao {
+public class JpaUserDao implements UserDAO{
 
-    public void persist(UserEntity user) {
+    @Override
+    public void save(UserEntity user) {
+        if (user == null) throw new IllegalArgumentException("User cannot be null");
+
         EntityManager em = MariaDbJpaConnection.createEntityManager();
-
         try {
             em.getTransaction().begin();
-            em.persist(user);
-
+            if (user.getId() == null) {
+                em.persist(user);
+            } else {
+                em.merge(user);
+            }
             em.getTransaction().commit();
         } catch (Exception e) {
             if (em.getTransaction().isActive()) {
@@ -26,42 +32,76 @@ public class UserDao {
         }
     }
 
+    @Override
     public UserEntity findById(Long id) {
         EntityManager em = MariaDbJpaConnection.createEntityManager();
         try {
-            em.getTransaction().begin();
+            return em.find(UserEntity.class, id);
         } finally {
             em.close();
         }
     }
 
+    @Override
     public UserEntity findByUsername(String username) {
         EntityManager em = MariaDbJpaConnection.createEntityManager();
         try {
-            em.getTransaction().begin();
+            TypedQuery<UserEntity> query = em.createQuery("Select u from UserEntity u where u.username = :username", UserEntity.class);
+            query.setParameter("username", username);
+            List<UserEntity> result = query.getResultList();
 
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            if (em.getTransaction().isActive()) {
-                em.getTransaction().rollback();
-            }
-            throw new RuntimeException("Failed to find user", e);
+            return result.isEmpty() ? null : result.get(0);
         } finally {
             em.close();
         }
     }
 
+    @Override
     public UserEntity findByEmail(String email) {
         EntityManager em = MariaDbJpaConnection.createEntityManager();
-        return id;
+        try {
+            TypedQuery<UserEntity> query = em.createQuery("Select e from UserEntity e where e.email = :email", UserEntity.class);
+            query.setParameter("email", email);
+            List<UserEntity> result = query.getResultList();
+
+            return result.isEmpty() ? null : result.get(0);
+        } finally {
+            em.close();
+        }
     }
 
-    public UserEntity deleteById(Long id) {
+    @Override
+    public void update(UserEntity user) {
+        if (user == null) throw new IllegalArgumentException("User cannot be null");
 
+        EntityManager em = MariaDbJpaConnection.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.merge(user);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            throw new RuntimeException("Failed to update user", e);
+        } finally {
+            em.close();
+        }
     }
 
+    @Override
+    public void delete(UserEntity user) {
+        if (user == null) throw new IllegalArgumentException("User cannot be null");
 
-    public UserEntity updateById(Long id) {
-
+        EntityManager em = MariaDbJpaConnection.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            UserEntity managedUser = em.merge(user);
+            em.remove(managedUser);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            throw new RuntimeException("Failed to delete user" + user, e);
+        } finally {
+            em.close();
+        }
     }
 }
