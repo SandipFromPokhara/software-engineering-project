2b1fbb11815b00ca787b173d34aae8e3f96b157f
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-02-05 13:29:30 +0200

    Update email field and passwordHash instead of plain password

--- src/main/java/model/UserModel.java
+++ src/main/java/model/UserModel.java
@@ -2,21 +2,24 @@ package model;
 
 public class UserModel {
     private int id;
-    private static int idCounter = 1;
     private String firstName;
     private String lastName;
     private String username;
-    private String password;
+    private String email;
+    private String passwordHash;
 
-    public UserModel(String firstName, String lastName, String username, String password) {
-        id = idCounter++;
+    public UserModel(String firstName, String lastName, String username, String email, String passwordHash) {
+
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
-        this.password = password;
+        this.email = email;
+        this.passwordHash = passwordHash;
     }
 
     public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
 
     public String getFirstName() { return this.firstName; }
 
@@ -30,12 +33,16 @@ public class UserModel {
 
     public void setUsername(String username) { this.username = username; }
 
-    public String getPassword() { return this.password; }
+    public String getEmail() { return this.email;}
 
-    public void setPassword(String password) { this.password = password; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPasswordHash() { return this.passwordHash; }
+
+    public void setPassword(String passwordHash) { this.passwordHash = passwordHash; }
 
     @Override
     public String toString() {
-        return "UserModel {ID: " + id + ", Full Name: '" + firstName + " " + lastName + "', Username: '" + username + "'}";
+        return "UserModel {ID: " + id + ", Full Name: '" + firstName + " " + lastName + "', Username: '" + username + "', Email: '" + email + "'}";
     }
 }
