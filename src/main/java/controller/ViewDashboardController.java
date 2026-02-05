47e35409ed7eaaf4db74f5ca22f6a80e40b6e643
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-02-05 21:24:42 +0200

    Implement logout method

--- src/main/java/controller/ViewDashboardController.java
+++ src/main/java/controller/ViewDashboardController.java
@@ -1,11 +1,38 @@
 package controller;
 
+import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import util.NavigationUtil;
 
+
 public class ViewDashboardController {
+
+    @FXML
+    private TableView<?> notesTable;
+
+    @FXML
+    private TextArea noteTextArea;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    public void initialize() {
+        notesTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    }
+
     @FXML
     private void handleLogout(ActionEvent event) {
         NavigationUtil.navigateTo(event, "/FXML/entry.fxml", "Welcome", false);
+    }
+
+    public void handleDelete(ActionEvent actionEvent) {
+    }
+
+    public void handleOpenEditWindow(ActionEvent actionEvent) {
+
     }
 }
