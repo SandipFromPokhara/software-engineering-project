4d62ac5dbb55c45d48215d2bb45196cce5e4779f
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-02-05 19:39:32 +0200

    Initialize controller file for View Dashboard

--- src/main/java/controller/ViewDashboardController.java
+++ src/main/java/controller/ViewDashboardController.java
@@ -1,4 +1,11 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import util.NavigationUtil;
+
 public class ViewDashboardController {
+    @FXML
+    private void handleLogout(ActionEvent event) {
+        NavigationUtil.navigateTo(event, "/FXML/entry.fxml", "Welcome", false);
+    }
 }
