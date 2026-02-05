4be0843e86186013225f31061e4cd6346a68a45c
Swostika Lama <swostika.lama01@gmail.com>
2026-02-05 09:59:57 +0200

    Update GuestDashboard FXML layout for improved UI and adjust Main class launch method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@ import javafx.application.Application;
 import view.GuestDashboardView;
 public class Main {
     public static void main(String[] args) {
-//        GuestDashboardView.launch(GuestDashboardView.class);
-        Application.launch(TestEditNote.class);
+        Application.launch(GuestDashboardView.class);
+//        Application.launch(TestEditNote.class);
     }
 }
 
