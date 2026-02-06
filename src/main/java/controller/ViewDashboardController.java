1ca6d349aa0b307d9258683525639745380a1501
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-02-06 10:10:49 +0200

    Update: Initialize method with selected/not-selected logic, add navigate to Edit window as a Modal Popup(not replacing dashboard)

--- src/main/java/controller/ViewDashboardController.java
+++ src/main/java/controller/ViewDashboardController.java
@@ -1,26 +1,58 @@
 package controller;
 
+import entity.NoteEntity;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import util.NavigationUtil;
 
 
 public class ViewDashboardController {
 
     @FXML
-    private TableView<?> notesTable;
+    private Button deleteButton;
 
     @FXML
-    private TextArea noteTextArea;
+    private Button editButton;
+
+    @FXML
+    private TableView<NoteEntity> notesTable;
+
+    @FXML
+    private Label noteTitleLabel;
+
+    @FXML
+    private TextArea noteViewArea;
 
     @FXML
     private Button logoutButton;
 
     @FXML
     public void initialize() {
+        editButton.setDisable(true);
+        deleteButton.setDisable(true);
+        noteTitleLabel.setText("Select a note to view details");
+        noteViewArea.setText("");
+
+        notesTable.getSelectionModel().selectedItemProperty().addListener((observable, oldSelection, newSelection) -> {
+            if (newSelection != null) {
+                noteTitleLabel.setText(newSelection.getTitle());
+                noteViewArea.setText(newSelection.getContent());
+
+                editButton.setDisable(false);
+                deleteButton.setDisable(false);
+            } else {
+                noteTitleLabel.setText("Select a note to view details");
+                noteViewArea.clear();
+                editButton.setDisable(true);
+                deleteButton.setDisable(true);
+            }
+        });
         notesTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
     }
 
@@ -29,10 +61,13 @@ public class ViewDashboardController {
         NavigationUtil.navigateTo(event, "/FXML/entry.fxml", "Welcome", false);
     }
 
-    public void handleDelete(ActionEvent actionEvent) {
+    public void handleDelete(ActionEvent event) {
     }
 
-    public void handleOpenEditWindow(ActionEvent actionEvent) {
+    public void handleOpenEditWindow(ActionEvent event) {
+        Stage editStage = new Stage();
+        editStage.initModality(Modality.APPLICATION_MODAL);
 
+        NavigationUtil.navigateTo(event, "/FXML/EditPage.fxml", "Edit", true);
     }
 }
