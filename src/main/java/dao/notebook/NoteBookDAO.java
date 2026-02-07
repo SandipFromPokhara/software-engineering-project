cbd31e7a765c402bae146c16a51248afc1af5487
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-02-07 18:48:47 +0200

    Declare findByUser() method in NoteBookDAO interface

--- src/main/java/dao/notebook/NoteBookDAO.java
+++ src/main/java/dao/notebook/NoteBookDAO.java
@@ -2,8 +2,11 @@ package dao.notebook;
 
 import dao.baseDAO.GenericDAO;
 import entity.NoteBookEntity;
+import entity.UserEntity;
+
 import java.util.List;
 
 public interface NoteBookDAO extends GenericDAO<NoteBookEntity, Long> {
+    List<NoteBookEntity> findByUser(UserEntity user);
     List<NoteBookEntity> findByTitle(String title);
 }
