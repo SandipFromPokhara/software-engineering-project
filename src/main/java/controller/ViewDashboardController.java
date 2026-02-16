affae178601756e19b82d8bb1446cd23236430c3
Swostika-Lama <swostika.lama01@gmail.com>
2026-02-16 18:47:26 +0200

    Edit test (#29)
    * Add showAbout for information and dateFormatter for notes creation
    
    * Add dateFormatter method
    
    * Add extra null checks and robust try-catch exceptions
    
    * Add annotation field, fix width of TableColumns, and adjust text labels
    
    * Enable login with Enter keypress and fix stage on logout:
    - Add Enter key handling to trigger login action
    - Create new Stage for entry pages on logout to maintain fixed-size
      layout
    
    * Create new stage for entry pages on logout to maintain fized-size layout
    
    * Initiate JpaNoteDao object and save annotation update
    
    * Merge handleOpenEditWindow() from feature-dev, remove unused ActionEvent param
    
    * Merge handleOpenEditWindow() from feature-dev, remove unused ActionEvent param
    
    * Implement handleClose() method for MenuItem Close option
    
    * Add handleCreate and handleClose actions for New and Close MenuItem
    
    * Merge delete() method from feature-fdelete
    
    * Fix javaFX version from 24 to 21
    
    * Add handleDelete() onAction in MenuItem Delete
    
    * Implement new session class for Notebook
    
    * Move NoteSession and UserSession files to new session package
    
    * Utilize NotebookSession to setLastCreatedNotebook
    
    * Implement new loadNotebooks() method to auto-load notebooks in TableView after creation
    
    * Minor update
    
    * Merge from remote
    
    * feat: Update database password in persistence.xml and add maven-dependency-plugin to pom.xml
    
    * feat: Refactor EditNoteController to use dependency injection for NoteDAO and add unit tests for controller functionality
    
    * feat: Refactor EditNoteController to use dependency injection for NoteDAO and add unit tests for controller functionality
    
    * feat: Add PDF export functionality for notes and integrate PDF library
    
    * Update UI and added action to export as pdf
    
    ---------
    
    Co-authored-by: Sandip Ranjit <sandip.ranjit@metropolia.fi>

--- src/main/java/controller/ViewDashboardController.java
+++ src/main/java/controller/ViewDashboardController.java
@@ -1,5 +1,10 @@
 package controller;
 
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.AreaBreak;
+import com.itextpdf.layout.element.Paragraph;
 import dao.note.JpaNoteDao;
 import dao.note.NoteDAO;
 import dao.notebook.JpaNoteBookDao;
@@ -23,18 +28,20 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ViewDashboardController {
 
+    private static final Logger logger = Logger.getLogger(ViewDashboardController.class.getName());
     private NoteBookEntity activeNotebook;
     private NoteBookDAO notebookDao;
     private NoteDAO noteDao;
     private TagDAO tagDao;
-
-    @FXML
-    private BorderPane rootPane;
 
     @FXML
     private MenuButton userMenuButton;
@@ -248,21 +255,8 @@ public class ViewDashboardController {
     }
 
     @FXML
-    private void handleLogout(ActionEvent event) {
-        Window window = rootPane.getScene().getWindow();
-
-        Alert confirmDialog = new Alert(Alert.AlertType.CONFIRMATION);
-        confirmDialog.setTitle("Logout");
-        confirmDialog.setHeaderText("Are your sure you want to logout?");
-        confirmDialog.setContentText("Any unsaved changes may be lost!");
-        confirmDialog.initOwner(window);
-
-        ButtonType logout = new ButtonType("Logout");
-        ButtonType cancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
-
-        confirmDialog.getButtonTypes().setAll(logout, cancel);
-
-        Optional<ButtonType> result = confirmDialog.showAndWait();
+    private void handleLogout() {
+        Stage currentStage = (Stage) logoutBtn.getScene().getWindow();
 
         if (result.isPresent() && result.get() == logout) {
             Stage stage = (Stage) window;
@@ -349,14 +343,137 @@ public class ViewDashboardController {
     }
 
     @FXML
-    public void handleClose() {
+    public void handleClose(ActionEvent actionEvent) {
         Stage stage = (Stage) userMenuButton.getScene().getWindow();
         stage.close();
     }
+    @FXML
+    private void handleManageAccount(){
+
+    }
+
+
+
 
     @FXML
-    public void handleManageAccount() {
-        // Stage stage = (Stage) userMenuButton.getScene().getWindow();
-        // NavigationUtil.openWindow(stage, "/FXML/user_dashboard.fxml", "NoteVault - User Dashboard", true, true, null);
+    private void handleExport() {
+        if (activeNotebook == null) {
+            showWarning("No notebook selected.");
+            return;
+        }
+        ChoiceDialog<String> dialog = new ChoiceDialog<>(
+                "Selected Note",
+                "Selected Note",
+                "Entire Notebook"
+        );
+        dialog.setTitle("Export Options");
+        dialog.setHeaderText("Choose what to export:");
+        dialog.setContentText("Export:");
+
+        Optional<String> result = dialog.showAndWait();
+
+        if (result.isEmpty()) return;
+
+        if (result.get().equals("Selected Note")) {
+            exportSelectedNote();
+        } else {
+            exportEntireNotebook();
+        }
+    }
+
+    private void exportSelectedNote() {
+        NoteEntity selectedNote = notesTable.getSelectionModel().getSelectedItem();
+        if (selectedNote == null) {
+            showWarning("Please select a note to export.");
+            return;
+        }
+        List<NoteEntity> singleNoteList = List.of(selectedNote);
+        exportNotesToPdf(singleNoteList, selectedNote.getTitle());
+    }
+
+    private void exportEntireNotebook() {
+        List<NoteEntity> notes = noteDao.findByNotebook(activeNotebook); // active one not
+        if (notes.isEmpty()) {
+            showWarning("Notebook is empty.");
+            return;
+        }
+        exportNotesToPdf(notes, activeNotebook.getTitle());
+    }
+
+
+    private void exportNotesToPdf(List<NoteEntity> notes, String fileName) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Export as PDF");
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("PDF Files", "*.pdf")
+        );
+        fileChooser.setInitialFileName(fileName + ".pdf");
+
+        File file = fileChooser.showSaveDialog(
+                userMenuButton.getScene().getWindow());
+
+        if (file == null) return;
+
+        try {
+            PdfWriter writer = new PdfWriter(file.getAbsolutePath());
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+
+            for (int i = 0; i < notes.size(); i++) {
+                NoteEntity note = notes.get(i);
+                document.add(new Paragraph(note.getTitle())
+                        .setBold()
+                        .setFontSize(18));
+                document.add(new Paragraph(note.getContent()));
+
+                if (note.getAnnotation() != null &&
+                        !note.getAnnotation().isEmpty()) {
+
+                    document.add(new Paragraph("\nAnnotation:")
+                            .setBold());
+
+                    document.add(new Paragraph(note.getAnnotation()));
+                }
+
+                if (i < notes.size() - 1) {
+                    document.add(new AreaBreak());
+                }
+            }
+
+            document.close();
+            showInfo("Export successful!");
+
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Export failed", e);
+            showError("Export failed.");
+        }
+    }
+
+
+    private void showWarning(String message) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Warning");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(userMenuButton.getScene().getWindow());
+        alert.showAndWait();
+    }
+
+    private void showInfo(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(userMenuButton.getScene().getWindow());
+        alert.showAndWait();
+    }
+
+    private void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(userMenuButton.getScene().getWindow());
+        alert.showAndWait();
     }
 }
