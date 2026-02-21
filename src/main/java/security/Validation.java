293a12584a4a4031b53d312bd51fc2139c83ce27
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-02-21 15:11:54 +0200

    Simplify if block in line 36

--- src/main/java/security/Validation.java
+++ src/main/java/security/Validation.java
@@ -1,4 +1,4 @@
-package util;
+package security;
 
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
@@ -33,11 +33,7 @@ public class Validation {
         }
 
         // Validate password length
-        if (!validatePasswordStrength(password, messageLabel)) {
-            return false;
-        }
-
-        return true;
+        return validatePasswordStrength(password, messageLabel);
     }
 
     // Validates that all required fields are filled
