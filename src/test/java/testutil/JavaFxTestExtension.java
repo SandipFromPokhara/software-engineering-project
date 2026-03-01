d4da0650b2944164c4ea0610538841258a0256cd
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-03-01 11:15:06 +0200

    Refactor: ignore if JavaFX is already started, prevent race conditions

--- src/test/java/testutil/JavaFxTestExtension.java
+++ src/test/java/testutil/JavaFxTestExtension.java
@@ -13,18 +13,28 @@ public class JavaFxTestExtension implements BeforeAllCallback, ExtensionContext.
 
     @Override
     public void beforeAll(ExtensionContext context) {
-        if (!started) {
-            started = true;
-            CountDownLatch latch = new CountDownLatch(1);
-            Platform.startup(latch::countDown);
+        if (started) {
+            return;
+        }
+
+        synchronized (JavaFxTestExtension.class) {
+            if (started) {
+                return;
+            }
+
             try {
+                CountDownLatch latch = new CountDownLatch(1);
+                Platform.startup(latch::countDown);
+
                 if (!latch.await(5, TimeUnit.SECONDS)) {
                     throw new IllegalStateException("JavaFX Platform failed to start");
                 }
+            } catch (IllegalStateException ignored) {
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 throw new IllegalStateException("Interrupted while waiting for JavaFX Platform to start", e);
             }
+            started = true;
         }
     }
 
