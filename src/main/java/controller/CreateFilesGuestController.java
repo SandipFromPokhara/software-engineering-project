fbdc3e5304e58ed78d88cae20be0b2fac99de74a
Swostika Lama <swostika.lama01@gmail.com>
2026-03-21 18:27:43 +0200

    added english language for create notes for guest

--- src/main/java/controller/CreateFilesGuestController.java
+++ src/main/java/controller/CreateFilesGuestController.java
@@ -2,11 +2,20 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import util.Localization;
 
 public class CreateFilesGuestController {
+    @FXML
+    Label title;
+    @FXML
+    Label content;
+    @FXML
+    Label annotation;
+
     @FXML
     TextField titleBox;
 
@@ -34,6 +43,18 @@ public class CreateFilesGuestController {
         if (annotationBox != null) {
             annotationBox.clear();
         }
+
+    }
+
+    @FXML
+    public void initialize() {
+        title.textProperty().bind(Localization.bind("note.title_label"));
+        titleBox.textProperty().bind(Localization.bind("note.placeholder_title"));
+        content.textProperty().bind(Localization.bind("note.content_label"));
+        contentBox.textProperty().bind(Localization.bind("note.placeholder_content"));
+        annotation.textProperty().bind(Localization.bind("note.annotation_label"));
+        annotationBox.textProperty().bind(Localization.bind("note.placeholder_annotation"));
+        cancelButton.textProperty().bind(Localization.bind("note.cancel"));
 
     }
 
