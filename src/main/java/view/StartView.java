a202dbccd6ca4feea1cb59d57e2fedbc4dab6e94
Swostika Lama <swostika.lama01@gmail.com>
2026-03-22 08:51:35 +0200

    add localization in starview

--- src/main/java/view/StartView.java
+++ src/main/java/view/StartView.java
@@ -2,12 +2,13 @@ package view;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import util.Localization;
 import util.NavigationUtil;
 
 public class StartView extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        NavigationUtil.replaceScene(stage, "/FXML/entry.fxml", "Welcome to NoteVault", false);
+        NavigationUtil.replaceScene(stage, "/FXML/entry.fxml", Localization.get("entry.window_title"), false);
     }
 }
