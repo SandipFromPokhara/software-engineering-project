73b119483f88a4868f7c122c1c8593f35ce4dee4
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-04-04 20:28:59 +0300

    Override equals() and hashCode() for collection behavior correctness

--- src/main/java/entity/NoteTranslationEntity.java
+++ src/main/java/entity/NoteTranslationEntity.java
@@ -2,15 +2,9 @@ package entity;
 
 import jakarta.persistence.*;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Table(name="note_translation", uniqueConstraints = { @UniqueConstraint(columnNames = {"note_id", "language_code"}) })
-public class NoteTranslationEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class NoteTranslationEntity extends BaseTranslationEntity {
 
     @Column(name="title", nullable = false)
     private String title;
@@ -21,41 +15,17 @@ public class NoteTranslationEntity {
     @Column(name="annotation")
     private String annotation;
 
-    @Column(name="createdAt")
-    private LocalDateTime createdAt;
-
-    @Column(name="updatedAt")
-    private LocalDateTime updatedAt;
-
-    @Column(name="language_code", nullable = false)
-    private String langCode;
-
     @ManyToOne
     @JoinColumn(name = "note_id", nullable = false)
     private NoteEntity note;
 
-    @PrePersist
-    protected void onCreate() {
-        createdAt = LocalDateTime.now();
-        updatedAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        updatedAt = LocalDateTime.now();
-    }
-
     public NoteTranslationEntity() {}
-
-    public Long getId() { return id; }
 
     public String getTitle() { return title; }
 
     public String getContent() { return content; }
 
     public String getAnnotation() { return annotation; }
-
-    public String getLangCode() { return langCode; }
 
     public NoteEntity getNote() { return note; }
 
@@ -69,12 +39,31 @@ public class NoteTranslationEntity {
 
     public void setAnnotation(String newAnnotation) { this.annotation = newAnnotation; }
 
-    public void setLangCode(String langCode) { this.langCode = langCode; }
-
     public void setNote(NoteEntity note) { this.note = note; }
 
     @Override
     public String toString() {
         return title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NoteTranslationEntity))return false;
+
+        // cast to correct type
+        NoteTranslationEntity that = (NoteTranslationEntity) o;
+
+        if (getLangCode() == null || that.getLangCode() == null) return false;
+        if (getNote() == null || that.getNote() == null) return false;
+
+        return getLangCode().equals(that.getLangCode()) && getNote().equals(that.getNote());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (getNote() != null ? getNote().hashCode() : 0);
+        result = 31 * result + (getLangCode() != null ? getLangCode().hashCode() : 0);
+        return result;
     }
 }
