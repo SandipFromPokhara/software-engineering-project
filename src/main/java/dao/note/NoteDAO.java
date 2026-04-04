6ffc48a8568f8f93dbd7443c92f5d303fdc99544
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-04-04 17:15:38 +0300

    Add additional method to find by id and language for db localization

--- src/main/java/dao/note/NoteDAO.java
+++ src/main/java/dao/note/NoteDAO.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface NoteDAO extends GenericDAO <NoteEntity, Long> {
     List<NoteEntity> findByTitle(String title);
     List<NoteEntity> findByNotebook(NoteBookEntity notebook);
+
+    NoteEntity findById(Long id, String language);
 }
