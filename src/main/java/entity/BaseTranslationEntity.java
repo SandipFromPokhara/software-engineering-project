2617dabdf891f40d5b2f39bfcc907a43c5bb824f
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-04-04 20:19:36 +0300

    Implement BaseTranslationEntity to avoid redundant fields and logic

---
+++ src/main/java/entity/BaseTranslationEntity.java
@@ -0,0 +1,42 @@
+package entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class BaseTranslationEntity {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="language_code", nullable = false)
+    private String langCode;
+
+    @Column(name="createdAt")
+    private LocalDateTime createdAt;
+
+    @Column(name="updatedAt")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public Long getId() { return id; }
+
+    public String getLangCode() { return langCode; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+
+    public LocalDateTime getUpdatedAt() { return updatedAt; }
+
+    public void setLangCode(String langCode) { this.langCode = langCode; }
+}
