317bba827a9bb91f0ba57b40b4444f08dbd5c008
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2026-04-11 12:28:52 +0300

    Revert constructor to public and add nested comment for empty constructor

--- src/main/java/entity/entities/UserEntity.java
+++ src/main/java/entity/entities/UserEntity.java
@@ -34,7 +34,7 @@ public class UserEntity extends BaseEntity {
         this.email = email;
     }
 
-    protected UserEntity() {}
+    public UserEntity() {/* Prevent instantiation. JPA only */}
 
     public String getFirstName() { return firstName; }
 
